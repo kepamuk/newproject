9f7a93eec6e0f962513a9d7ae6884d41b68d30bc
kepamuk <kepamuk@yandex.ru>
2018-02-12 11:10:17 +0300

    hash2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { TestingRComponent } from './testing-r/testing-r.component';
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [{provide: LocationStrategy, useClass: PathLocationStrategy}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
